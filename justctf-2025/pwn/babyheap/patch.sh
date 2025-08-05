85ff90f1ac8bafafc2f7309dbb18281bbc938ea1
williamwijaya <66501624+d0UBleW@users.noreply.github.com>
2025-08-05 14:36:36 +0700

    chore: update patchelf options

--- justctf-2025/pwn/babyheap/patch.sh
+++ justctf-2025/pwn/babyheap/patch.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-patchelf --set-interpreter ld-linux-x86-64.so.2 \
+patchelf \
+    --no-sort \
+    --set-interpreter ld-linux-x86-64.so.2 \
     --replace-needed libc.so.6 ./libc.so.6 \
     --output babyheap.patched \
     babyheap
